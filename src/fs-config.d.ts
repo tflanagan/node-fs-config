cbb2b538511fe86c9fe1a9854449bb8a61800aba
Tristian Flanagan <tflanagan@datacollaborative.com>
2020-06-05 08:23:01 -0400

    src: fix types

--- src/fs-config.d.ts
+++ src/fs-config.d.ts
@@ -1,12 +1,12 @@
 declare class FSConfig {
 	public config: FSConfigObj;
     constructor();
-	async loadDir<T = FSConfigObj>(dir: string): Promise<T>;
+	loadDir<T = FSConfigObj>(dir: string): Promise<T>;
 	loadDirSync<T = FSConfigObj>(dir: string): T;
 	loadFile<T = FSConfigObj>(file: string, sync?: boolean): T;
 }
 
-export interface FSConfigObj {
+interface FSConfigObj {
 	[index: string]: any;
 }
 
